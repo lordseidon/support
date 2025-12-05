7a196229113b1a862531900a207c2d919f261cc4
Vecoyenka01 <miyasmiths234@gmail.com>
2025-12-05 17:14:45 +0100

    Now ui improved 3

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import ReactMarkdown from 'react-markdown';
 import { streamResponse } from '../services/geminiService';
 import './ChatBot.css';
+import favImage from '../img/fav.jpg';
 
 // Demo responses for when API is not configured
 const getDemoResponse = (userMessage) => {
@@ -368,9 +369,7 @@ const ChatBot = ({ isWidget = false }) => {
                       <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
                     </svg>
                   ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-                      <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM164.1 325.5C182 346.2 212.6 368 256 368s74-21.8 91.9-42.5c5.8-6.7 15.9-7.4 22.6-1.6s7.4 15.9 1.6 22.6C349.8 372.1 311.1 400 256 400s-93.8-27.9-116.1-53.5c-5.8-6.7-5.1-16.8 1.6-22.6s16.8-5.1 22.6 1.6zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
-                    </svg>
+                    <img src={favImage} alt="AI Avatar" className="avatar-image" />
                   )}
                 </div>
                 <div className="message-content-1">
